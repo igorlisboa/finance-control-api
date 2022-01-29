2d11cd5121708958078cf4c344662f481f2470f5
Igor Lisboa <igorlisboa34@gmail.com>
2022-01-29 12:13:36 -0300

    first commit

---
+++ src/main/java/br/com/financial/financecontrolapi/config/DataSourceConfig.java
@@ -0,0 +1,7 @@
+package br.com.financial.financecontrolapi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+}
---
+++ src/test/java/br/com/financial/financecontrolapi/FinanceControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financial.financecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
